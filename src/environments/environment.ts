cc78d583b31d790749c127a79f25e48ac75fe3f8
Rin Minase <rin.black.raison.detre@gmail.com>
2019-04-11 00:50:38 +0800

    Added firebase configuration in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
-	production: false
+	production: false,
+
+	firebaseConfig: {
+		apiKey: "AIzaSyCew6V3ZjYb39RmtAr35j87P9ryKtP6ysw",
+		authDomain: "ng-todolist-fire.firebaseapp.com",
+		databaseURL: "https://ng-todolist-fire.firebaseio.com",
+		projectId: "ng-todolist-fire",
+		storageBucket: "ng-todolist-fire.appspot.com",
+		messagingSenderId: "774052209024"
+	},
 };
