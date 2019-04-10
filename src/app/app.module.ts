01d4cdd997d99dfc4b0ba5e0f3a039d8c2ce841a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-04-11 00:50:22 +0800

    Added firestore modules in main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { environment } from '@env/environment';
 
 const routes: Routes = [{
 	path: "",
@@ -18,6 +21,9 @@ const routes: Routes = [{
 		BrowserModule,
 		RouterModule.forRoot(routes),
 		BrowserAnimationsModule,
+
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule,
 
 		HomeModule,
 	],
