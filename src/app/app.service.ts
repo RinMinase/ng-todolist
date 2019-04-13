fdcc1a4dcdda5131aafdb5c1d2636df696931498
Rin Minase <rin.black.raison.detre@gmail.com>
2019-04-13 22:28:19 +0800

    Fixed status flag on adding

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -11,7 +11,7 @@ export class AppService {
 				.collection("todo")
 				.add({
 					deleted: false,
-					status: true,
+					status: false,
 					value: data,
 					timestamp: new Date().getTime(),
 				})
